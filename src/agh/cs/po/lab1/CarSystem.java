ee480e05e1d0c016a1507f12bd351b74a93ef0f8
MatiX <matixpl99@gmail.com>
2019-10-06 11:55:51 +0200

    Initial commit

---
+++ src/agh/cs/po/lab1/CarSystem.java
@@ -0,0 +1,33 @@
+package agh.cs.po.lab1;
+
+import java.sql.SQLOutput;
+
+public class CarSystem
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Start");
+        run(Direction.toDirs(args));
+        System.out.println("Stop");
+    }
+
+    public static void run(Direction[] dirs)
+    {
+        for (Direction dir : dirs)
+        {
+            if (dir.equals(Direction.FORWARD))
+                System.out.println("Going forward...");
+            else if (dir.equals(Direction.LEFT))
+                System.out.println("Going backward...");
+            else if (dir.equals(Direction.RIGHT))
+                System.out.println("Going right...");
+            else if (dir.equals(Direction.BACKWARD))
+                System.out.println("Going left...");
+            else
+            {
+                System.out.println("Wrong input!");
+                break;
+            }
+        }
+    }
+}
---
+++ src/agh/cs/po/lab1/Direction.java
@@ -0,0 +1,29 @@
+package agh.cs.po.lab1;
+
+public enum Direction
+{
+    FORWARD, LEFT, RIGHT, BACKWARD, NAN;
+
+    static Direction fromString(String arg)
+    {
+        if (arg.equals("f"))
+            return Direction.FORWARD;
+        else if (arg.equals("l"))
+            return Direction.LEFT;
+        else if (arg.equals("r"))
+            return Direction.RIGHT;
+        else if (arg.equals("b"))
+            return Direction.BACKWARD;
+        else
+            return Direction.NAN;
+    }
+    public static Direction[] toDirs(String[] args)
+    {
+        Direction[] dirs = new Direction[args.length];
+        for (int i = 0; i < args.length; i++)
+        {
+            dirs[i] = Direction.fromString(args[i]);
+        }
+        return dirs;
+    }
+}
