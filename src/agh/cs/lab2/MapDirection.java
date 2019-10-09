0a50c6df295c520d495a9ad1b2ef688ab24391f4
MatiX <matixpl99@gmail.com>
2019-10-09 12:40:05 +0200

    lab2

---
+++ src/agh/cs/lab2/MapDirection.java
@@ -0,0 +1,66 @@
+package agh.cs.lab2;
+
+import java.util.Vector;
+
+public enum MapDirection
+{
+    NORTH, EAST, SOUTH, WEST;
+
+    public String toString()
+    {
+        if (this == null)
+            return "ERROR";
+        else if (this == MapDirection.NORTH)
+            return "Polnoc";
+        else if (this == MapDirection.SOUTH)
+            return "Poludnie";
+        else if (this == MapDirection.WEST)
+            return "Zachod";
+        else //if (dir == MapDirection.EAST)
+            return "Wschod";
+    }
+
+    public MapDirection next()
+    {
+        if (this == null)
+            return null;
+        else if (this == MapDirection.NORTH)
+            return MapDirection.EAST;
+        else if (this == MapDirection.EAST)
+            return MapDirection.SOUTH;
+        else if (this == MapDirection.SOUTH)
+            return MapDirection.WEST;
+        else //if (dir == MapDirection.WEST)
+            return MapDirection.NORTH;
+    }
+
+    public MapDirection previous()
+    {
+        if (this == null)
+            return null;
+        else if (this == MapDirection.NORTH)
+            return MapDirection.WEST;
+        else if (this == MapDirection.WEST)
+            return MapDirection.SOUTH;
+        else if (this == MapDirection.SOUTH)
+            return MapDirection.EAST;
+        else //if (dir == MapDirection.EAST)
+            return MapDirection.NORTH;
+    }
+
+    public Vector2d toUnitVector(MapDirection dir)
+    {
+        if (this == null)
+            return null;
+        else if (this == MapDirection.NORTH)
+            return new Vector2d(0, 1);
+        else if (this == MapDirection.EAST)
+            return new Vector2d(-1, 0);
+        else if (this == MapDirection.SOUTH)
+            return new Vector2d(0, -1);
+        else //if (this == MapDirection.WEST)
+            return new Vector2d(1, 0);
+    }
+
+
+}
---
+++ src/agh/cs/lab2/Vector2d.java
@@ -0,0 +1,79 @@
+package agh.cs.lab2;
+
+public class Vector2d
+{
+    public final int x;
+    public final int y;
+
+    public Vector2d(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString()
+    {
+        String result;
+        result = "(" + Integer.toString(this.x) + "," + Integer.toString(this.y) + ")";
+        return result;
+    }
+
+    public boolean precedes(Vector2d other)
+    {
+        if (this.x >= other.x && this.y >= other.y)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean follows(Vector2d other)
+    {
+        if (this.x <= other.x && this.y <= other.y)
+            return true;
+        else
+            return false;
+    }
+
+    public Vector2d upperRight(Vector2d other)
+    {
+        return new Vector2d(Math.max(this.x, other.x), Math.max(this.y, other.y));
+    }
+
+    public Vector2d lowerRight(Vector2d other)
+    {
+        return new Vector2d(Math.min(this.x, other.x), Math.min(this.y, other.y));
+    }
+
+    public Vector2d add(Vector2d other)
+    {
+        return new Vector2d(this.x + other.x, this.y + other.y);
+    }
+
+    public Vector2d subtract(Vector2d other)
+    {
+        return new Vector2d(this.x - other.x, this.y - other.y);
+    }
+
+    public boolean equals(Object other)
+    {
+        if (this == other)
+            return true;
+        if (!(other instanceof Vector2d))
+            return false;
+        Vector2d that = (Vector2d) other;
+        if (this.x == that.x && this.y == that.y)
+            return true;
+        else
+            return false;
+    }
+
+    Vector2d opposite()
+    {
+        return new Vector2d(-this.x, -this.y);
+    }
+
+
+
+
+
+}
